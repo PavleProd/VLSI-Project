66421e55c0ccf1df2931a970165bcd9a261e07cc
Павле Продановић <96532387+PavleProd@users.noreply.github.com>
2024-06-24 01:45:17 +0200

    add memory module documentation

--- src/synthesis/modules/memory.v
+++ src/synthesis/modules/memory.v
@@ -1,15 +1,18 @@
 module memory #(
-	parameter FILE_NAME = "mem_init.mif",
-    parameter ADDR_WIDTH = 6,
-    parameter DATA_WIDTH = 16
+	parameter FILE_NAME = "mem_init.mif", // fajl iz kog se inicijalizuje memorija
+    parameter ADDR_WIDTH = 6, // velicina memorije 2^ADDR_WIDTH
+    parameter DATA_WIDTH = 16 // velicina memorijske reci
 )(
     input clk,
-    input we,
-    input [ADDR_WIDTH - 1:0] addr,
-    input [DATA_WIDTH - 1:0] data,
-    output reg [DATA_WIDTH - 1:0] out
+    input we, // write enable: 0 - citanje, 1 - upis
+    input [ADDR_WIDTH - 1:0] addr, // ako je we==0, sa ove adrese se cita, inace se upisuje
+    input [DATA_WIDTH - 1:0] data, // ako je we==1, ovaj podatak se upisuje na addr
+    output reg [DATA_WIDTH - 1:0] out // upisuje se uvek podatak sa adrese addr(novoupisani ako je upis)
 );
 
+    // podrazumevano 2^6 ulaza. Prvih 8 lokacija: GPR registri.
+    // Ostale lokacije: slobodna zona podataka (program, podaci, stek)
+    // STEK: poslednja memorijska lokacija, raste ka nizim adresama
 	(* ram_init_file = FILE_NAME *) reg [DATA_WIDTH - 1:0] mem [2**ADDR_WIDTH - 1:0];
 
     always @(posedge clk) begin
